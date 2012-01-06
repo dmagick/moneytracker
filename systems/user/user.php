cf904ce0183088b4feda6912f78896c05f1e7cee
Chris Smith <dmagick@gmail.com>
2012-01-06 14:21:28 +1100

    phpdocs comments for the user system

--- systems/user/user.php
+++ systems/user/user.php
@@ -1,13 +1,44 @@
 <?php
+/**
+ * User class file.
+ *
+ * @author Chris Smith <dmagick@gmail.com>
+ * @version 1.0
+ * @package money
+ */
 
+/**
+ * The user class.
+ * Handles authentication, checking the user hasn't locked their
+ * account, setting tokens.
+ * Anything user related.
+ *
+ * @package money
+ */
 class user
 {
     /**
      * Number of minutes to lock someone out from too many
      * attempts to log in.
+     *
+     * @static
      */
     private static $_lockTimeLimit = 5;
 
+    /**
+     * This does all the work when viewed in a browser.
+     * It displays the appropriate template (with keyword replacements).
+     *
+     * @uses template::display
+     * @uses template::serveTemplate
+     * @uses template::setKeyword
+     * @uses user::setToken
+     * @uses user::authCheck
+     *
+     * @return void
+     *
+     * @static
+     */
     public static function process()
     {
         template::serveTemplate('user.header');
@@ -26,7 +57,26 @@ class user
     }
 
     /**
-     * Authentication checking goes here.
+     * Check the user hasn't locked themselves out.
+     * If they have, the process is stopped quickly.
+     * After that, checks the appropriate options are filled in
+     * (username/password and there is a session token set).
+     * Then finally checks the values match the db.
+     *
+     * @static
+     *
+     * @uses session::get
+     * @uses session::set
+     * @uses session::setFlashMessage
+     * @uses template::serveTemplate
+     * @uses template::setKeyword
+     * @uses url::redirect
+     * @uses user::setToken
+     * @uses user::_isLockedOut
+     *
+     * @return void
+     *
+     * @static
      */
     private static function authCheck()
     {
@@ -89,6 +139,23 @@ class user
         return;
     }
 
+    /**
+     * Checks whether the user has tried to log in too many times
+     * in the last time period.
+     * If $update is FALSE, returns a boolean based on the number of attempts.
+     * If $update is TRUE, it either starts a counter for the ip
+     * based on the current time, or updates the counter it already has,
+     * and then returns nothing.
+     *
+     * @param boolean $update Whether to try updating the db (TRUE) -
+     *                        this is also the default,
+     *                        or just see if they have locked themselves out.
+     *
+     * @return mixed Returns a boolean if update is FALSE, returns void if
+     *               update is TRUE.
+     *
+     * @static
+     */
     private static function _isLockedOut($update=TRUE)
     {
         $ip     = self::_getIp();
@@ -126,6 +193,15 @@ class user
         $result = db::execute($sql, $values);
     }
 
+    /**
+     * Gets the ip from the users browser.
+     * Checks for X_FORWARDED_FOR in case they are behind a proxy.
+     * If that's not available, uses REMOTE_ADDR
+     *
+     * @return string The users ip.
+     *
+     * @static
+     */
     private static function _getIp()
     {
         $ip = '';
@@ -138,6 +214,14 @@ class user
         return trim($ip);
     }
 
+    /**
+     * Set a unique token in the session.
+     * Also returns it for the login page to use.
+     *
+     * @return string The random token for the login page to use.
+     *
+     * @static
+     */
     private static function setToken()
     {
         $token = sha1(uniqid(rand(), TRUE));
@@ -145,8 +229,25 @@ class user
         return $token;
     }
 
-    private static function checkLoginDetails($username, $password)
+    /**
+     * Check whether a username/password combination match the database.
+     * If they do, it will also delete any user_login_locks the user has.
+     * If they don't, throws an exception.
+     *
+     * @param string $username The username to try.
+     * @param string $password The password to try.
+     *
+     * @return integer The user_id of the user if the username/password match.
+     * @throws exception Throws an exception if the details don't match.
+     *
+     * @static
+     */
+    private static function checkLoginDetails($username=NULL, $password=NULL)
     {
+        if ($username === NULL || $password === NULL) {
+            throw new Exception("Unable to authenticate user");
+        }
+
         $sql   = "select user_id from ".db::getPrefix()."users where username=:username and passwd=:password and useractive='y'";
         $query = db::select($sql, array($username, sha1($password)));
         $user  = db::fetch($query);
