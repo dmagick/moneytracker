e528bb5590f084efcf6384324e0fd9dd41098872
Chris Smith <dmagick@gmail.com>
2012-01-06 14:59:26 +1100

    phpdocs comments for the url system. also added a vim tag at the bottom for consistent style

--- systems/url/url.php
+++ systems/url/url.php
@@ -1,20 +1,55 @@
 <?php
+/**
+ * URL class file.
+ *
+ * @author Chris Smith <dmagick@gmail.com>
+ * @version 1.0
+ * @package money
+ */
 
+/**
+ * The url class.
+ * Handles redirecting to a particular page prefixed with the system url.
+ *
+ * @package money
+ */
 class url
 {
+
+    /**
+     * The system url. Redirecting is done based on this (with a page
+     * name at the end).
+     *
+     * @uses url::getUrl
+     * @uses url::redirect
+     * @uses url::setUrl
+     *
+     * @static
+     */
 	private static $_url = NULL;
 
 	/**
 	 * Redirect to another page with a header redirect.
 	 *
-	 * @param string $page The page to redirect to.
+	 * @param string  $page  The page to redirect to.
 	 * @param boolean $admin Whether it's in the admin area or not.
 	 *
+     * @uses getUrl
+     *
 	 * @return void
+     * @throw  exception Throws an exception if the url hasn't been
+     *                   set previously.
+     *
+     * @static
 	 */
 	public static function redirect($page, $admin=FALSE)
 	{
-		$url = self::getUrl().'/';
+        try {
+            $url = self::getUrl().'/';
+        } catch (Exception $e) {
+			throw new Exception("Url has not been set");
+        }
+
 		if ($admin === TRUE) {
 			$url .= 'admin/';
 		}
@@ -24,12 +59,30 @@ class url
 		exit;
 	}
 
+    /**
+     * Sets the url in the class.
+     *
+     * @param string $url The url to use.
+     *
+     * @return void
+     *
+     * @static
+     */
 	public static function setUrl($url)
 	{
 		self::$_url = $url;
 	}
 
-	public static function getUrl()
+    /**
+     * Gets the url from the class.
+     *
+     * @return string    Returns the url.
+     * @throws exception Throws an exception if the url
+     *                   hasn't been set before.
+     *
+     * @static
+     */
+    public static function getUrl()
 	{
 		if (self::$_url === NULL) {
 			throw new Exception("Url has not been set");
@@ -38,3 +91,5 @@ class url
 		return self::$_url;
 	}
 }
+
+/* vim: set expandtab ts=4 sw=4: */
